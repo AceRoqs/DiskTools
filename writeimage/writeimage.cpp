c8dbd3d28f1b0f7a8fe414cfa10ab4267e66441c
Toby Jones <ace_roqs@yahoo.com>
2014-10-05 22:53:24 -0700

    Basic writing of an empty disk.

--- writeimage/writeimage.cpp
+++ writeimage/writeimage.cpp
@@ -1,6 +1,8 @@
 #include <Windows.h>
 #include <tchar.h>
 #include <cstdint>
+#include <fstream>
+#include <vector>
 
 #pragma pack(push, 1)
 struct Bios_parameter_block
@@ -34,6 +36,18 @@ int _tmain(int argc, _In_count_(argc) PTSTR* argv)
 
     // ERRORLEVEL zero is the success code.
     int error_level = 0;
+
+    const unsigned int bytes_per_sector = 512;
+    const unsigned int sides = 2;
+    const unsigned int tracks_per_side = 80;
+    const unsigned int sectors_per_track = 18;
+    std::vector<uint8_t> disk_image(bytes_per_sector * sides * tracks_per_side * sectors_per_track);
+
+    // Writing to ofstream is much faster than writing to basic_ofstream<uint8_t>, but for the
+    // sizes being written, it's okay in optimized builds.
+    std::basic_ofstream<uint8_t> output_file("disk.img");
+    output_file.write(&disk_image[0], disk_image.size());
+    output_file.close();
 
     return error_level;
 }
