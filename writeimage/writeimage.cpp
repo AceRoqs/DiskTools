87217a128050544fd01eb7a81bacf1ca9a47e229
Toby Jones <ace_roqs@yahoo.com>
2014-10-07 23:39:26 -0700

    First version that outputs a fully valid disk image.

--- writeimage/writeimage.cpp
+++ writeimage/writeimage.cpp
@@ -34,49 +34,70 @@ std::vector<uint8_t> get_default_boot_sector()
 {
     std::vector<uint8_t> boot_sector(bytes_per_sector);
 
+    // TODO: write jump, bpb, and boot code.
+    boot_sector[bytes_per_sector - 2] = 0x55;
+    boot_sector[bytes_per_sector - 1] = 0xaa;
+
     return std::move(boot_sector);
 }
 
-std::vector<uint8_t> get_empty_file_allocation_table()
+std::vector<uint8_t> get_empty_file_allocation_table(unsigned int sector_count)
 {
-    std::vector<uint8_t> file_allocation_table;
+    std::vector<uint8_t> file_allocation_table(sector_count * bytes_per_sector);
+
+    file_allocation_table[0] = 0xf0;
+    file_allocation_table[1] = 0xff;
+    file_allocation_table[2] = 0xff;
 
     return std::move(file_allocation_table);
 }
 
-std::vector<uint8_t> get_empty_root_directory()
+std::vector<uint8_t> get_empty_root_directory(unsigned int sector_count)
 {
-    std::vector<uint8_t> root_directory;
+    std::vector<uint8_t> root_directory(sector_count * bytes_per_sector);
 
     return std::move(root_directory);
 }
 
 int _tmain(int argc, _In_count_(argc) PTSTR* argv)
 {
-    UNREFERENCED_PARAMETER(argc);
-    UNREFERENCED_PARAMETER(argv);
-
     // ERRORLEVEL zero is the success code.
     int error_level = 0;
+
+    std::wstring boot_sector_file_name;
+    for(int ii = 0; ii < argc; ++ii)
+    {
+        if(_tcsncmp(argv[ii], _T("-b="), 3) == 0)
+        {
+            boot_sector_file_name = &argv[ii][3];
+        }
+    }
+
+    auto boot_sector = get_default_boot_sector();
+    const auto file_allocation_table = get_empty_file_allocation_table(9);
+    const auto root_directory = get_empty_root_directory(14);
+
+    if(!boot_sector_file_name.empty())
+    {
+        std::basic_ifstream<uint8_t> boot_sector_file(boot_sector_file_name, std::ios::in | std::ios::binary);
+        boot_sector_file.read(&boot_sector[0], boot_sector.size());
+    }
 
     const unsigned int sides = 2;
     const unsigned int tracks_per_side = 80;
     const unsigned int sectors_per_track = 18;
     std::vector<uint8_t> disk_image(bytes_per_sector * sides * tracks_per_side * sectors_per_track);
 
-    const auto boot_sector = get_default_boot_sector();
-    const auto file_allocation_table = get_empty_file_allocation_table();
-    const auto root_directory = get_empty_root_directory();
-
     auto iterator = std::begin(disk_image);
     iterator = std::copy(std::cbegin(boot_sector), std::cend(boot_sector), iterator);
     iterator = std::copy(std::cbegin(file_allocation_table), std::cend(file_allocation_table), iterator);
     iterator = std::copy(std::cbegin(file_allocation_table), std::cend(file_allocation_table), iterator);
-    std::copy(std::cbegin(root_directory), std::cend(root_directory), iterator);
+    iterator = std::copy(std::cbegin(root_directory), std::cend(root_directory), iterator);
+    std::fill(iterator, std::end(disk_image), 0xf6);    // Match behavior of bfi.exe, by Bart Lagerweij.
 
     // Writing to ofstream is much faster than writing to basic_ofstream<uint8_t>, but for the
     // sizes being written, it's okay in optimized builds.
-    std::basic_ofstream<uint8_t> output_file("disk.img");
+    std::basic_ofstream<uint8_t> output_file("disk.img", std::ios::out | std::ios::binary);
     output_file.write(&disk_image[0], disk_image.size());
     output_file.close();
 
