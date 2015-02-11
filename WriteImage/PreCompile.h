2b705c63c409841893404854f60a29619189f586
Toby Jones <ace_roqs@yahoo.com>
2015-02-10 19:43:11 -0800

    WriteImage skeleton.

---
+++ WriteImage/PreCompile.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Windows.h>
+#include <tchar.h>
+#include <algorithm>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <tuple>
+#include <vector>
+
---
+++ WriteImage/WriteImage.cpp
@@ -0,0 +1,17 @@
+#include "PreCompile.h"
+
+namespace WriteImage
+{
+}
+
+int wmain(int argc, _In_reads_(argc) PWSTR* argv)
+{
+    UNREFERENCED_PARAMETER(argc);
+    UNREFERENCED_PARAMETER(argv);
+
+    // ERRORLEVEL zero is the success code.
+    int error_level = 0;
+
+    return error_level;
+}
+
