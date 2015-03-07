b9d3b1ec1cd48c87b49f145f6297425f70816311
Toby Jones <ace_roqs@yahoo.com>
2015-03-06 23:43:13 -0800

    New export for UTF-8 fprintf.

--- PlatformServices/Shell.cpp
+++ PlatformServices/Shell.cpp
@@ -41,5 +41,13 @@ std::vector<std::string> get_utf8_args(int argc, _In_reads_(argc) char** argv)
     return args;
 }
 
+int fprintf_utf8(_In_ FILE* stream, _In_z_ const char* format, ...)
+{
+    UNREFERENCED_PARAMETER(stream);
+    UNREFERENCED_PARAMETER(format);
+
+    return 0;
+}
+
 }
 
