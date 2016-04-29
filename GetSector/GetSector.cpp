9df58a835275a5c96e4fb04acc65848ea03f3c64
Toby Jones <ace_roqs@yahoo.com>
2016-04-29 10:14:36 -0700

    Convert GetSector to use a more exception-friendly style (like CryptoTools).

--- GetSector/GetSector.cpp
+++ GetSector/GetSector.cpp
@@ -1,12 +1,70 @@
 #include "PreCompile.h"
 #include <DiskTools/DirectRead.h>
+#include <WindowsCommon/DebuggerTracing.h>
+#include <PortableRuntime/CheckException.h>
+#include <PortableRuntime/Tracing.h>
+#include <PortableRuntime/Unicode.h>
 
-int _tmain(int argc, _In_reads_(argc) PTSTR* argv)
+namespace DiskTools
 {
-    const unsigned int arg_program_name = 0;
-    const unsigned int arg_sector       = 1;
-    const unsigned int arg_output_file  = 2;
 
+int read_sector_to_file(_In_z_ const wchar_t* output_file_name, uint64_t sector_number);
+
+}
+
+int wmain(int argc, _In_reads_(argc) wchar_t** argv)
+{
+    constexpr auto arg_program_name     = 0;
+    constexpr auto arg_sector_number    = 1;
+    constexpr auto arg_output_file_name = 2;
+
+    // ERRORLEVEL zero is the success code.
+    int error_level = 0;
+
+    try
+    {
+        PortableRuntime::set_dprintf(WindowsCommon::debugger_dprintf);
+
+        // Set wprintf output to UTF-8 in Windows console.
+        // CHECK_EXCEPTION ensures against the case that the CRT invalid parameter handler
+        // routine is set by a global constructor.
+        CHECK_EXCEPTION(_setmode(_fileno(stdout), _O_U8TEXT) != -1, u8"Failed to set UTF-8 output mode.");
+        CHECK_EXCEPTION(_setmode(_fileno(stderr), _O_U8TEXT) != -1, u8"Failed to set UTF-8 output mode.");
+
+        if(argc == 3)
+        {
+            // There is no _wtoui64 function (and perhaps a private implementation is a good idea), but
+            // reading an int64_t into a uint64_t will have no negative (ha!) consequences, as any sector
+            // number is considered a valid sector to read.
+            const uint64_t sector_number = _wtoi64(argv[arg_sector_number]);
+            error_level = DiskTools::read_sector_to_file(argv[arg_output_file_name], sector_number);
+        }
+        else
+        {
+            std::fwprintf(stderr, L"Usage: %s sector file_name\n", argv[arg_program_name]);
+            std::fwprintf(stderr, L"To read the Master Boot Record: %s 1 mbr.bin\n", argv[arg_program_name]);
+            error_level = 1;
+        }
+    }
+    catch(const std::exception& ex)
+    {
+        std::fwprintf(stderr, L"\n%s\n", PortableRuntime::utf16_from_utf8(ex.what()).c_str());
+        error_level = 1;
+    }
+
+    return error_level;
+}
+
+namespace DiskTools
+{
+
+_Use_decl_annotations_
+int read_sector_to_file(const wchar_t* output_file_name, uint64_t sector_number)
+{
+    // TODO: 2016: This error_level is temporary, as error should be reported by exceptions.
+    int error_level = 0;
+
+    // TODO: 2016: Should the sector size be configurable?
     const unsigned int sector_size = 512;
 
     std::array<uint8_t, sector_size> buffer;
@@ -14,39 +72,33 @@ int _tmain(int argc, _In_reads_(argc) PTSTR* argv)
     assert(buffer.size() < UINT_MAX);
     unsigned int buffer_size = static_cast<unsigned int>(buffer.size());
 
-    if(3 != argc)
+    // TODO: 2016: Convert this to a throwing function.
+    if(SUCCEEDED(DiskTools::read_sector_from_disk(buffer.data(), &buffer_size, 0, sector_number)))
     {
-        _tprintf(_TEXT("Usage: %s sector file_name\r\n"), argv[arg_program_name]);
-        _tprintf(_TEXT("To read MBR: %s 1 mbr.bin\r\n"), argv[arg_program_name]);
-        return 0;
-    }
-
-    // ERRORLEVEL zero is the success code.
-    int error_level = 0;
-
-    if(SUCCEEDED(DiskTools::read_sector_from_disk(buffer.data(), &buffer_size, 0, _ttoi64(argv[arg_sector]))))
-    {
-        std::unique_ptr<FILE, int (*)(FILE*)> file(_tfopen(argv[arg_output_file], _TEXT("wb")), std::fclose);
+        // TODO: 2016: Convert this to a ofstream, since it has proper exception-safe lifetime semantics.
+        std::unique_ptr<FILE, int (*)(FILE*)> file(_wfopen(output_file_name, L"wb"), std::fclose);
         if(0 == file)
         {
-            _ftprintf(stderr, _TEXT("%s: error opening %s.\r\n"), argv[arg_program_name], argv[arg_output_file]);
+            std::fwprintf(stderr, L"Error opening %s.\n", output_file_name);
             error_level = 1;
         }
         else
         {
             if(std::fwrite(buffer.data(), 1, buffer_size, file.get()) != buffer_size)
             {
-                _ftprintf(stderr, _TEXT("%s: error writing %s.\r\n"), argv[arg_program_name], argv[arg_output_file]);
+                std::fwprintf(stderr, L"Error writing %s.\n", output_file_name);
                 error_level = 1;
             }
         }
     }
     else
     {
-        _ftprintf(stderr, _TEXT("%s: error reading sector.\r\n"), argv[arg_program_name]);
+        std::fwprintf(stderr, L"Error reading sector.\n");
         error_level = 1;
     }
 
     return error_level;
+}
+
 }
 
--- GetSector/PreCompile.h
+++ GetSector/PreCompile.h
@@ -1,9 +1,19 @@
 #pragma once
 
 #include <cassert>
-#include <cstdint>
 #include <array>
+#include <cstdint>
 #include <memory>
+
+#ifdef _MSC_VER
+
+// TODO: 2016: Remove all instances of tchar.h.
 #include <tchar.h>
 #include <windows.h>
+
+// APIs for MSVCRT UTF-8 output.
+#include <fcntl.h>
+#include <io.h>
+
+#endif
 
