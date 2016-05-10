907309b062bc935ebbb9d8e9885314554e008618
Toby Jones <ace_roqs@yahoo.com>
2016-05-09 22:00:19 -0700

    main contains only command line parsing and reporting.

--- RipISO/RipISO.cpp
+++ RipISO/RipISO.cpp
@@ -9,6 +9,59 @@
 #include <PortableRuntime/Unicode.h>
 #include <PlatformServices/Shell.h>
 
+namespace RipISO
+{
+    void rip_iso(_In_z_ const char* output_file_name)
+    {
+        const auto disk_handle = WindowsCommon::create_file(
+            DiskTools::get_file_name_cdrom_0(),
+            GENERIC_READ,
+            FILE_SHARE_READ,
+            nullptr,
+            OPEN_EXISTING,
+            FILE_ATTRIBUTE_NORMAL,
+            nullptr);
+
+        const auto output_file = WindowsCommon::create_file(
+            output_file_name,
+            GENERIC_WRITE,
+            0,
+            nullptr,
+            CREATE_ALWAYS,
+            FILE_ATTRIBUTE_NORMAL,
+            nullptr);
+
+        DWORD bytes_returned;
+        GET_LENGTH_INFORMATION length_information;
+        CHECK_BOOL_LAST_ERROR(DeviceIoControl(disk_handle,
+                                              IOCTL_DISK_GET_LENGTH_INFO,
+                                              nullptr,
+                                              0,
+                                              &length_information,
+                                              sizeof(length_information),
+                                              &bytes_returned,
+                                              nullptr) != 0);
+        const unsigned int buffer_size = 1024 * 1024;
+        std::unique_ptr<uint8_t[]> buffer = std::make_unique<uint8_t[]>(buffer_size);
+
+        ULONGLONG bytes_left = length_information.Length.QuadPart;
+        while(bytes_left > 0)
+        {
+            // Cast is safe as buffer_size is less than MAX_DWORD.
+            DWORD amount_to_read = bytes_left > buffer_size ? buffer_size : static_cast<DWORD>(bytes_left);
+
+            // This is reasonably slow, but it is fast enough for single CDs or DVDs.
+            // A fast approach might be to use uncached aligned async reads, at the
+            // expense of considerable complexity.
+            DWORD amount_read;
+            CHECK_BOOL_LAST_ERROR(ReadFile(disk_handle, buffer.get(), amount_to_read, &amount_read, nullptr) != 0);
+            CHECK_BOOL_LAST_ERROR(WriteFile(output_file, buffer.get(), amount_read, &amount_read, nullptr) != 0);
+
+            bytes_left -= amount_to_read;
+        }
+    }
+}
+
 int main(int argc, _In_reads_(argc) char** argv)
 {
     // ERRORLEVEL zero is the success code.
@@ -30,52 +83,7 @@ int main(int argc, _In_reads_(argc) char** argv)
         const auto args = PlatformServices::get_utf8_args(argc, argv);
         if(args.size() == 2)
         {
-            const auto disk_handle = WindowsCommon::create_file(
-                DiskTools::get_file_name_cdrom_0(),
-                GENERIC_READ,
-                FILE_SHARE_READ,
-                nullptr,
-                OPEN_EXISTING,
-                FILE_ATTRIBUTE_NORMAL,
-                nullptr);
-
-            const auto output_file = WindowsCommon::create_file(
-                args[arg_output_file].c_str(),
-                GENERIC_WRITE,
-                0,
-                nullptr,
-                CREATE_ALWAYS,
-                FILE_ATTRIBUTE_NORMAL,
-                nullptr);
-
-            DWORD bytes_returned;
-            GET_LENGTH_INFORMATION length_information;
-            CHECK_BOOL_LAST_ERROR(DeviceIoControl(disk_handle,
-                                                  IOCTL_DISK_GET_LENGTH_INFO,
-                                                  nullptr,
-                                                  0,
-                                                  &length_information,
-                                                  sizeof(length_information),
-                                                  &bytes_returned,
-                                                  nullptr) != 0);
-            const unsigned int buffer_size = 1024 * 1024;
-            std::unique_ptr<uint8_t[]> buffer = std::make_unique<uint8_t[]>(buffer_size);
-
-            ULONGLONG bytes_left = length_information.Length.QuadPart;
-            while(bytes_left > 0)
-            {
-                // Cast is safe as buffer_size is less than MAX_DWORD.
-                DWORD amount_to_read = bytes_left > buffer_size ? buffer_size : static_cast<DWORD>(bytes_left);
-
-                // This is reasonably slow, but it is fast enough for single CDs or DVDs.
-                // A fast approach might be to use uncached aligned async reads, at the
-                // expense of considerable complexity.
-                DWORD amount_read;
-                CHECK_BOOL_LAST_ERROR(ReadFile(disk_handle, buffer.get(), amount_to_read, &amount_read, nullptr) != 0);
-                CHECK_BOOL_LAST_ERROR(WriteFile(output_file, buffer.get(), amount_read, &amount_read, nullptr) != 0);
-
-                bytes_left -= amount_to_read;
-            }
+            RipISO::rip_iso(args[arg_output_file].c_str());
         }
         else
         {
